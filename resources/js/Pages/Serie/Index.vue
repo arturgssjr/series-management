2beedcd245b26a50347e4a5e33efb2ab9e7dbddc
Artur Júnior <artur.junior@soluti.com.br>
2021-04-28 20:32:47 -0300

    update gitignore

---
+++ resources/js/Pages/Serie/Index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="serie in series" :key="serie.id">{{ serie.describe.name }}
+                <Seasons :serie="serie"/>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import Seasons from "@/Pages/Series/Seasons";
+
+export default {
+    name: "Index",
+    components: {Seasons},
+    props: {
+        series: Object
+    }
+}
+</script>
+
---
+++ resources/js/Pages/Serie/Show.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        {{ series.describe.name }}
+        <Seasons :serie="series"/>
+    </div>
+</template>
+
+<script>
+import Seasons from "@/Pages/Series/Seasons";
+
+export default {
+    name: "Show",
+    components: {Seasons},
+    props: {
+        series: Object
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
